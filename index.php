7fb82c760c9904e5f038178d5e015d93298e139a
M Teguh A Suandi <teguh.andro@gmail.com>
2013-11-01 10:18:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,13 @@
 <?php
+/**
+ * Format array proxies:
+ * array('host' => 'HOST', 'port' => 'PORT', 'username' => 'USERNAME', 'password' => 'PASSWORD');
+ */
 include("RankChecker.class.php");
-$RankChecker = new RankChecker(1,5);
-
-for($res = 10; $res<=40; $res += 10)
-{
-	set_time_limit(90);
-	$result = $RankChecker->find("com", $res, "imlaunchr.com","viral coupon popup");
-	if ($result !== false)
-	{
-		echo "Page: ".$result["page"].".";
-		echo "<br />Link: <a href='".$result["url"]."'>".$result["url"]."</a>";
-		echo "<br />Position: ".$result["position"];
-		echo "<hr />";
-	}
-	else
-	{
-		echo "Not found!";
-	}
-}
-
-for($res = 50; $res<=100; $res += 50)
-{
-	set_time_limit(90);
-	$result = $RankChecker->find("com", 100, "imlaunchr.com","viral coupon popup");
-	if ($result !== false)
-	{
-		echo "Page: ".$result["page"].".";
-		echo "<br />Link: <a href='".$result["url"]."'>".$result["url"]."</a>";
-		echo "<br />Position: ".$result["position"];
-		echo "<hr />";
-	}
-	else
-	{
-		echo "Not found!";
-	}
-}
+$newGoogleRankChecker 	= new GoogleRankChecker();
+$newquery 		= 'YOUR_KEYWORD';
+$useproxies 		= 'TRUE_OR_FALSE';
+$arrayproxies 		= 'ARRAY_PROXIES';
+$googledata 		= $newGoogleRankChecker->find($newquery, $useproxies, $arrayproxies);
+var_dump($googledata);
 ?>
