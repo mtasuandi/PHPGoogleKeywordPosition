bab683489023cc37a84451c1eceade6728d550b8
M Teguh A Suandi <teguh.andro@gmail.com>
2013-11-01 10:14:23 +0700

    Update RankChecker.class.php
    Google changed their html code from <cite> to <cite class="vurls">.

--- RankChecker.class.php
+++ RankChecker.class.php
@@ -18,7 +18,9 @@ Change Log:
 		Second release, add keyword position result.
 ************************************************************************************************************************
 */
-ini_set('max_execution_time', 1200);
+if(@ini_set('max_execution_time', 1200) !== FALSE)
+	@ini_set('max_execution_time', 1200);
+
 if(!class_exists('GoogleRankChecker'))
 {
 	class GoogleRankChecker
@@ -26,129 +28,193 @@ if(!class_exists('GoogleRankChecker'))
 		public $start;
 		public $end;
 		
-		public function __construct($start=1, $end=10)
+		public function __construct($start=1, $end=2)
 		{
 			$this->start	= $start;
 			$this->end		= $end;
 		}
 		
-		public function find($keyword)
-		{
-			for($start = ($this->start-1)*10; $start < $this->end*10; $start += 10)
+		public function find($keyword, $useproxie, $proxies)
+		{	
+			for($start = ($this->start-1)*10; $start <= $this->end*10; $start += 10)
 			{
 				$ua	= array(
 					0 	=> 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
 					10 	=> 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
-					20 	=> 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
-					30  => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36',
-					40  => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36',
-					50  => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
-					60  => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0',
-					70  => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36',
-					80  => 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
-					90  => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'
+					20 	=> 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1'
 				);
 	
-				$options = array(
-				  	'http'		=> array(
-				    	'method'	=> "GET",
-				    	'header'	=> "Accept-language: en\r\n" .
-				            "Cookie: foo=bar\r\n" . 
-				            "User-Agent: " . $ua[$start]
-				    )
-				);
+				if($useproxie)
+				{				
+					$host 		= $proxies["host"];
+					$port		= $proxies["port"];
+					$username	= $proxies["username"];
+					$password 	= $proxies["password"];
+					
+					if(!empty($username))
+					{
+						$auth 		= base64_encode($username.":".$password);
+						$useauth 	= "Proxy-Authorization: Basic $auth";
+					}
+					else
+					{
+						$useauth = "";
+					}
+					
+					$options	= array(
+						"http"		=> array(
+							"method"			=> 	"GET",
+							"header"			=> 	"Accept-language: en\r\n".
+													"Cookie: biztech=indonesia\r\n". 
+													"User-Agent: ".$ua[$start]."\r\n".
+													$useauth,
+							"proxy" 			=> 	"tcp://".$host.":".$port,
+							"request_fulluri" 	=> 	true
+						)
+					);
+				}
+				else
+				{
+					$options = array(
+						"http"		=> array(
+							"method"	=> "GET",
+							"header"	=> "Accept-language: en\r\n" .
+								"Cookie: biztech=indonesia\r\n" . 
+								"User-Agent: ".$ua[$start])
+					);
+				}
+				
+				if($useproxie)
+				{
+					if(!empty($username))
+					{
+						$auth 	= base64_encode($username.":".$password);
+		
+						$arrayproxies	= array(
+							CURLOPT_PROXY 			=> $host,
+							CURLOPT_PROXYPORT		=> $port,
+							CURLOPT_PROXYUSERPWD 	=> $auth
+						);
+					}
+					else
+					{
+						$arrayproxies	= array(
+							CURLOPT_PROXY 		=> $host,
+							CURLOPT_PROXYPORT	=> $port
+						);
+					}
+				}
+				else
+				{
+					$arrayproxies	= array();
+				}
 				
 				$keyword		= str_replace(" ", "+", trim($keyword));
-				$url			= "https://www.google.com/search?ie=UTF-8&q=$keyword&start=$start&num=100";
-				sleep(10);
+				$url			= "https://www.google.com/search?ie=UTF-8&q=$keyword&start=$start&num=30";
 				$context 		= stream_context_create($options);
 
 				if($this->_isCurl())
 				{
-					$data 	= $this->_curl($url);
+					$data 	= $this->_curl($url, $useproxie, $arrayproxies);
 				}
 				else
 				{
-					$data	= file_get_contents($url, false, $context);
+					$data	= @file_get_contents($url, false, $context);
 				}
 				
-				$flag	= false;
-				$j		= -1;
-				$i 		= 1;
-				
-				while( ($j = stripos($data,"<cite>",$j+1)) !== false )
+				if(is_array($data))
 				{
-					$k 			= stripos($data,"</cite>",$j);
-					$link 		= strip_tags(substr($data,$j,$k-$j));
-					$rank		= $i++;
-					$results[]	= array("rank" => $rank, "url" => $link);
-
-					if($this->_isCurl() == false)
+					$errmsg 	= $data['errmsg'];
+					$results 	= array("rank" => "zerox", "url" => $errmsg);
+				}
+				else
+				{
+					if(strpos($data, "To continue, please type the characters below") !== FALSE || $data == FALSE || strpos($data, "We're sorry") !== FALSE)
 					{
-						$flag 	= true;
+						$results 	= array("rank" => "zero", "url" => "");
+					}
+					else
+					{
+						$flag	= false;
+						$j		= -1;
+						$i 		= 1;
+						
+						while( ($j = stripos($data,'<cite class="vurls">',$j+1)) !== false )
+						{
+							$k 			= stripos($data,"</cite>",$j);
+							$link 		= strip_tags(substr($data,$j,$k-$j));
+							$rank		= $i++;
+							$results[]	= array("rank" => 1, "url" => $link);
+		
+							if($this->_isCurl() === false)
+							{
+								$flag 	= true;
+							}
+						}
+						
+						if ($flag) {
+							break;
+						}
 					}
 				}
 				
-				if ($flag) {
-					break;
+				$sleep = rand(20,25);
+				sleep($sleep);
+				
+				if($this->_isCurl() === false)
+				{
+					return $results;
 				}
 			}
-			return $results;
-		}
-
-		public function get_web_page($url)
-		{
-			$res = array();
-		    $options = array( 
-		        CURLOPT_RETURNTRANSFER => true,
-		        CURLOPT_HEADER         => false,
-		        CURLOPT_FOLLOWLOCATION => true, 
-		        CURLOPT_USERAGENT      => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1',
-		        CURLOPT_AUTOREFERER    => true, 
-		        CURLOPT_CONNECTTIMEOUT => 120, 
-		        CURLOPT_TIMEOUT        => 120, 
-		        CURLOPT_MAXREDIRS      => 10, 
-		    ); 
-
-		    $newurl	 = 'http://www.checkpagerankapi.com/pr?url='.$url; 
-		    $ch      = curl_init( $newurl ); 
-		    curl_setopt_array( $ch, $options );
-		    set_time_limit(240); 
-		    $content = curl_exec( $ch ); 
-		    $err     = curl_errno( $ch ); 
-		    $errmsg  = curl_error( $ch ); 
-		    $header  = curl_getinfo( $ch ); 
-		    curl_close( $ch ); 
-   
-		    $res['url'] 	= $header['url'];
-		    return $res; 
+			
+			if($this->_isCurl())
+			{
+				return $results;
+			}
 		}
 
 		private function _isCurl()
 		{
-		    return function_exists('curl_version');
+		    return function_exists("curl_version");
 		}
 
-		private function _curl($url)
-		{
-			$curloptions = array( 
-		        CURLOPT_RETURNTRANSFER => true,
-		        CURLOPT_HEADER         => false,
-		        CURLOPT_FOLLOWLOCATION => true, 
-		        CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
-		        CURLOPT_AUTOREFERER    => true, 
-		        CURLOPT_CONNECTTIMEOUT => 120, 
-		        CURLOPT_TIMEOUT        => 120, 
-		        CURLOPT_MAXREDIRS      => 10,
-		        CURLOPT_SSL_VERIFYPEER => false 
-		    );
-
-			$ch      = curl_init($url); 
-		    curl_setopt_array($ch, $curloptions);
-		    set_time_limit(240); 
-		    $content = curl_exec($ch);
+		private function _curl($url, $useproxie, $arrayproxies)
+		{			
+			$ch = curl_init($url); 
+		    
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36");
+			curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+			curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+			if($useproxie)
+			{
+				if(!empty($arrayproxies))
+				{
+					foreach($arrayproxies as $param => $val)
+					{	
+						curl_setopt($ch, $param, $val);
+					}
+				}
+			}
+		    $content 	= curl_exec($ch);
+			$errno 		= curl_errno($ch);
+			$error 		= curl_error($ch);
 		    curl_close($ch);
-		    return $content;
+			if($errno == 0)
+			{
+				return $content;
+			}
+			else
+			{
+				return array("errno" => $errno, "errmsg" => $error);
+			}
+		    
 		}
 	}
 }
